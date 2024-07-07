f613f63f604706dccb9ac026ca375f00a199ca3e
ZedolsPython <rastislav.radocha@gmail.com>
2024-07-07 12:25:28 +0200

    Website status checker

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ import requests
 from fake_useragent import UserAgent
 from http import HTTPStatus
 
-
 def get_websites(csv_path: str) -> list[str]:
-    websites: list[str] = []
-    with open(csv_path, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if 'https://' not in row[1]:
-                websites.append(f'https://{row[1]}')
             else:
-                websites.append(row[1])
 
-        return websites
 
 
 def get_user_agent() -> str:
